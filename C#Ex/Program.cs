bdc324ab1f5512040a69224d4383df0345b77e9b
Caio Henrique <caioadriana5@gmail.com>
2021-06-10 15:29:22 -0300

    Conslusão dos exercicios 1 e 2

---
+++ C#Ex/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace C_Ex
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int Nota;
+
+            do
+            {
+                Console.WriteLine("Por favor insira sua nota (0/10)");
+                Nota = int.Parse(Console.ReadLine());
+
+                if (Nota >= 0 && Nota <= 10)
+                {
+
+                    Console.WriteLine("Sua nota é " + Nota);
+
+                }
+                else
+                {
+                    Console.WriteLine("Sua nota " + Nota + " é invalida.");
+                }
+            } while (Nota >= 10);
+
+
+
+
+        }
+    }
+}
---
+++ C#Ex2/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace C_Ex2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           string senhaUsuario;
+           string nomeUsuario;
+            do
+            {
+                Console.WriteLine("Escolha um nome de Usuário");
+                nomeUsuario = Console.ReadLine();
+
+                Console.WriteLine("Crie uma senha"); 
+                senhaUsuario = Console.ReadLine();
+
+
+                if (nomeUsuario == senhaUsuario)
+                {
+
+                    Console.WriteLine("Sua senha não pode ser igual ao Usuário, por favor escola outra senha.");
+            
+                }
+                else
+                {
+                    Console.WriteLine("Usuário e senha registrados com sucesso.");
+                
+                }
+            }  while (nomeUsuario == senhaUsuario);
+        }
+    }
+}
